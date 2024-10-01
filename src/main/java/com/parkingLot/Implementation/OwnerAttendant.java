25742c92066192f1d74b03cd97dd3db714d6a726
pridhi.1_ftc <pridhi.1_ftc@external.swiggy.in>
2024-10-01 15:32:47 +0530

    Added Policeman & Owner

--- src/main/java/com/parkingLot/Implementation/OwnerAttendant.java
+++ src/main/java/com/parkingLot/Implementation/OwnerAttendant.java
@@ -3,8 +3,11 @@ package com.parkingLot.Implementation;
 import com.parkingLot.Interface.Attendant;
 import com.parkingLot.Interface.Notifiable;
 import com.parkingLot.entities.ParkingLot;
+import com.parkingLot.entities.Policeman;
 import com.parkingLot.entities.Ticket;
 import com.parkingLot.entities.Vehicle;
+import com.parkingLot.exceptions.NoEmptySlotException;
+import com.parkingLot.exceptions.VehicleAlreadyParkedException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +17,7 @@ public class OwnerAttendant implements Attendant, Notifiable {
 
     private String ownerID;
     private List<ParkingLot> ownedParkingLots = new ArrayList<>();
+    private List<Vehicle> parkedVehiclesList = new ArrayList<>();
     private List<ParkingLot> ownerAssignedParkingLots = new ArrayList<>();
 
     public OwnerAttendant() {
@@ -27,10 +31,32 @@ public class OwnerAttendant implements Attendant, Notifiable {
         return lot;
     }
 
-    public Ticket park(Vehicle vehicle) {
+    public Ticket park(Vehicle vehicle) throws Exception{
 
 
-        return null;
+        if (vehicle == null) {
+            throw new Exception("Vehicle cannot be null");
+        }
+        if (!parkedVehiclesList.contains(vehicle)) {
+            for (ParkingLot parkingLot : assignedParkingLots) {
+                if (parkingLot.numberOfVacantSlots() > 0) {
+                    Ticket returnedTicket = parkingLot.park(vehicle);
+
+                    Policeman notify = new Policeman();
+                    if (parkingLot.numberOfVacantSlots() == 0) {
+                        notify.notifyParkingFull(parkingLot);
+                    } else {
+                        notify.notifyParkingAvailable(parkingLot);
+                    }
+
+                    parkedVehiclesList.add(vehicle);
+                    return returnedTicket;
+                }
+            }
+            throw new NoEmptySlotException("No empty slot available in any of the assigned parking lots");
+        } else {
+            throw new VehicleAlreadyParkedException("Vehicle is already parked");
+        }
     }
 
     @Override
